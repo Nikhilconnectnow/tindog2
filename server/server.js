afce1c3c431ebaecf98d6f9f44d5ddeaa26145f8
Nikhil <nikhil.connectnow@gmail.com>
2025-06-06 12:22:15 +0530

    FIXING API

--- server/server.js
+++ server/server.js
@@ -10,12 +10,23 @@ connectDB();
 
 const allowedOrigin = process.env.FRONTEND_URL || 'http://localhost:3000';
 
-app.use(
-  cors({
-    origin: allowedOrigin,
-    credentials: true,
-  })
-);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', allowedOrigin);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200); 
+  }
+  
+  next();
+});
+
+app.use(cors({
+  origin: allowedOrigin,
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
