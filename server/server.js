d13190a2ba2825a6dd9fb81e258891dde3a87575
Nikhil <nikhil.connectnow@gmail.com>
2025-06-06 11:40:35 +0530

    setting up for productin

--- server/server.js
+++ server/server.js
@@ -8,7 +8,12 @@ const app = express();
 dotenv.config();
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
