d21a245c194394c1e05c98089ab71ca652c60f13
Nikhil <nikhil.connectnow@gmail.com>
2025-06-07 09:30:45 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,27 +8,23 @@ const app = express();
 dotenv.config();
 connectDB();
 
-const allowedOrigin = process.env.FRONTEND_URL || 'http://localhost:3000';
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', allowedOrigin);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200); 
-  }
-  
-  next();
-});
-
+const allowedOrigins = [
+  process.env.FRONTEND_URL,            
+  'http://localhost:5173',              
+  'http://localhost:3000'              
+];
 app.use(cors({
-  origin: allowedOrigin,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
 }));
-app.use(express.json());
 
+app.use(express.json());
 app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
